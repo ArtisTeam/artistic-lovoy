9501b254bbd42b200fd4ee757da08ef735397b94
Yixing Lao <linuxthink@gmail.com>
2015-03-27 16:28:45 -0700

    two indent

--- cloud/app.js
+++ cloud/app.js
@@ -11,19 +11,19 @@ app.use(express.bodyParser());    // Middleware for reading request body
 // This is an example of hooking up a request handler with a specific request
 // path and HTTP verb using the Express routing API.
 app.get('/login', function(req, res) {
-    res.render('login', { message: 'Congrats, you just set up your app!' });
+  res.render('login', { message: 'Congrats, you just set up your app!' });
 });
 
 // Example reading from the request query string of an HTTP get request.
 app.get('/login', function (req, res) {
-    // GET http://example.parseapp.com/test?message=hello
-    res.send(req.query.message);
+  // GET http://example.parseapp.com/test?message=hello
+  res.send(req.query.message);
 });
 
 // Example reading from the request body of an HTTP post request.
 app.post('/login', function (req, res) {
-    // POST http://example.parseapp.com/test (with request body "message=hello")
-    res.send(req.body.message);
+  // POST http://example.parseapp.com/test (with request body "message=hello")
+  res.send(req.body.message);
 });
 
 // Attach the Express app to Cloud Code.
