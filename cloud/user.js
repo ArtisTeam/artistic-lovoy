78d67677a8b7283a1f8ff5fdc312a1599c7bdde3
JW-CHEN <jic023@eng.ucsd.edu>
2015-03-29 22:23:41 -0700

    create volunteer rows in database

--- cloud/user.js
+++ cloud/user.js
@@ -12,8 +12,8 @@ module.exports = function () {
     user.set('username', req.body.username);
     user.set('password', req.body.password);
     user.set('email', req.body.email);
-    user.set('group', req.body.group);
-    alert(req.body.group);
+    var group = parseInt(req.body.group);
+    user.set('group', group);
     user.signUp(null, {
       success: function (user) {
         //---use this to create new attributes
@@ -22,21 +22,26 @@ module.exports = function () {
         //    this.orgName = 'noname';
         //  }
         //});
-        var OrgProfile = Parse.Object.extend('OrgProfile');
-        var orgProf = new OrgProfile();
-        var orgProfACL = new Parse.ACL();
-        orgProfACL.setPublicReadAccess(true);
-        orgProfACL.setPublicWriteAccess(false);
-        orgProfACL.setWriteAccess(Parse.User.current(), true);
-        orgProf.setACL(orgProfACL);
+        if (group === 1) {
+          var profile = Parse.Object.extend('OrgProfile');
+        }
+        else {
+          var profile = Parse.Object.extend('VolProfile');
+        }
+        var prof = new profile();
+        var profACL = new Parse.ACL();
+        profACL.setPublicReadAccess(true);
+        profACL.setPublicWriteAccess(false);
+        profACL.setWriteAccess(Parse.User.current(), true);
+        prof.setACL(profACL);
 
         //orgProf.set('orgName', 'UCSD'); //TODO: let user change this
-        orgProf.set('createBy', Parse.User.current());
-        orgProf.save(null, {
-          success: function (orgProf) {
+        prof.set('createBy', Parse.User.current());
+        prof.save(null, {
+          success: function (prof) {
             res.redirect('/dashboard');
           },
-          error: function (orgProf, error) {
+          error: function (prof, error) {
             alert('Failed to create new OrgProfile, with error code: ' + error.message);
             res.send('Failed to create OrgProfile, with error code: ' + error.message);
           }
