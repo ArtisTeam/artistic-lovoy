74aef5fe0bb80cdbac4a6d425522a8df1f095227
Yixing Lao <linuxthink@gmail.com>
2015-04-07 16:04:21 -0700

    middleware authentication for vol and org, simplifies event.js

--- cloud/event.js
+++ cloud/event.js
@@ -1,7 +1,16 @@
-// The order is important
+// [Notes on security]
+// 1. The order is important
+// 2. Use next() to fix order and middleware problem
+// 3. Three kinds of access rights for middleware
+//    > global: only GET /:id
+//    > require login
+//      > require vol: /enroll/*
+//      > require org: otherthing
+// 
 // GET     /new         render event/new (done)
 // POST    /new         submit form create new event (done)
-// GET     /:id         render event/detail
+// GET     /:id         render event/detail (the only thing that can be viewed
+//                                           without login)
 // POST    /:id/delete  delete event
 // GET     /:id/edit    render event/edit
 // POST    /:id/edit    updeate event
@@ -12,43 +21,13 @@
 module.exports = function () {
   var express = require('express');
   var app = express();
+  var currUser = null;
 
-  // render event/new
-  app.get('/new', function (req, res) {
-    res.render('event/new'); // must not include '/' in front
-  });
-
-  // submit form create new event
-  app.post('/new', function (req, res) {
-    var currUser = Parse.User.current();
-    if (currUser && currUser.get('group') === 1) {
-      var EventItem = Parse.Object.extend('Event');
-      var eventItem = new EventItem();
-      eventItem.set('createdBy', currUser); // pointer to user
-      eventItem.set('eventName', req.body.eventName);
-      eventItem.set('eventDescription', req.body.eventDescription);
-
-      var acl = new Parse.ACL();
-      acl.setPublicReadAccess(true);
-      acl.setPublicWriteAccess(false);
-      acl.setWriteAccess(currUser, true);
-      eventItem.setACL(acl);
-
-      eventItem.save(null, {
-        success: function (event) {
-          res.redirect('/dashboard');
-        },
-        error: function (event, error) {
-          res.send('Failed to save event, with error code: ' + error.message);
-        }
-      });
-    } else {
-      res.redirect('/login');
+  // render event/detail, the only view not require login
+  app.get('/:id', function (req, res, next) {
+    if (req.params.id === 'new') {
+      next();
     }
-  });
-
-  // render event/detail
-  app.get('/:id', function (req, res) {
     var Event = Parse.Object.extend('Event');
     var query = new Parse.Query(Event);
     query.get(req.params.id, {
@@ -61,97 +40,141 @@ module.exports = function () {
     });
   });
 
-  // delete event
-  // to optimize: use the same middleware for post delete and edit
-  app.post('/:id/delete', function (req, res) {
-    var currUser = Parse.User.current();
-    if (currUser && currUser.get('group') === 1) { // logged in as org
-      var Event = Parse.Object.extend('Event');
-      var query = new Parse.Query(Event);
-      alert('req.params.id' + req.params.id)
-      query.equalTo('createdBy', currUser); // crated by curr user
-      query.get(req.params.id, {
-        success: function (event) {
-          event.destroy({
-            success: function (event) {
-              alert('success destroied')
-              res.redirect('/dashboard');
-            },
-            error: function (event, error) {
-              res.send('Failed to save event, error code: ' + error.message);
-            }
-          });
-        },
-        error: function (error) {
-          res.redirect('/dashboard');
-        }
-      });
+  // middleware, require whichever login, set current user
+  app.all('*', function (req, res, next) {
+    currUser = Parse.User.current();
+    if (currUser) {
+      alert("whichever middleware accepted");
+      next();
     } else {
-      res.redirect('/login');
-    }
-    alert("delete " + req.params.id);
-  });
-  
-  // render event/edit
-  app.get('/:id/edit', function (req, res) {
-    var currUser = Parse.User.current();
-    if (currUser && currUser.get('group') === 1) {
-      var Event = Parse.Object.extend('Event');
-      var query = new Parse.Query(Event);
-      query.equalTo('createdBy', currUser);
-      query.get(req.params.id, {
-        success: function (event) {
-          res.render('event/edit', {event: event});
-        },
-        error: function (event, error) {
-          res.redirect('/dashboard');
-        }
-      });
-    } else {
+      alert("whichever middleware rejected, redirect to login");
       res.redirect('/login');
     }
   });
 
-  // update event
-  app.post('/:id/edit', function (req, res) {
-    var currUser = Parse.User.current();
-    if (currUser && currUser.get('group') === 1) { // logged in as org
-      var Event = Parse.Object.extend('Event');
-      var query = new Parse.Query(Event);
-      alert('req.params.id' + req.params.id)
-      query.equalTo('createdBy', currUser); // crated by curr user
-      query.get(req.params.id, {
-        success: function (event) {
-          alert('success find to post edit')
-          event.set('eventName', req.body.eventName);
-          event.set('eventDescription', req.body.eventDescription);
-          event.save(null, {
-            success: function (event) {
-              res.redirect('/dashboard');
-            },
-            error: function (event, error) {
-              res.send('Failed to save event, with error code: ' + error.message);
-            }
-          });
-        },
-        error: function (event, error) {
-          res.redirect('/dashboard');
-        }
-      });
+  // middle-ware, require loged in as volunteer when enroll/unenroll
+  app.all('/:id/enroll*', function (req, res, next) {
+    if (currUser.get('group') === 2) {
+      alert("vol middleware accepted");
+      next();
     } else {
-      res.redirect('/login');
+      alert("vol middleware rejected, redirect to dashboard");
+      res.redirect('/dashboard');
     }
   });
 
   // enroll in event - shall we use post or get?
-  app.post('/:id/enroll', function (req, res) {
+  app.get('/:id/enroll', function (req, res) {
     alert("enroll " + req.params.id);
   });
 
   // unenroll in event
-  app.post('/:id/enroll/delete', function (req, res) {
+  app.get('/:id/enroll/delete', function (req, res) {
     alert("unenroll " + req.params.id);
   });
 
+  // middle-ware, for anything else, require organization logged in
+  app.all('*', function (req, res, next) {
+    if (currUser.get('group') === 1) {
+      alert("org middleware accepted");
+      next();
+    } else {
+      alert("org middleware rejected, redirect to dashboard");
+      res.redirect('/dashboard');
+    }
+  });
+
+  // render event/new
+  app.get('/new', function (req, res) {
+    res.render('event/new'); // must not include '/' in front
+  });
+
+  // submit form create new event
+  app.post('/new', function (req, res) {
+    var EventItem = Parse.Object.extend('Event');
+    var eventItem = new EventItem();
+    eventItem.set('createdBy', currUser); // pointer to user
+    eventItem.set('eventName', req.body.eventName);
+    eventItem.set('eventDescription', req.body.eventDescription);
+
+    var acl = new Parse.ACL();
+    acl.setPublicReadAccess(true);
+    acl.setPublicWriteAccess(false);
+    acl.setWriteAccess(currUser, true);
+    eventItem.setACL(acl);
+
+    eventItem.save(null, {
+      success: function (event) {
+        res.redirect('/dashboard');
+      },
+      error: function (event, error) {
+        res.send('Failed to save event, with error code: ' + error.message);
+      }
+    });
+  });
+
+  // delete event
+  // to optimize: use the same middleware for post delete and edit
+  app.post('/:id/delete', function (req, res) {
+    var Event = Parse.Object.extend('Event');
+    var query = new Parse.Query(Event);
+    query.equalTo('createdBy', currUser); // crated by curr user
+    query.get(req.params.id, {
+      success: function (event) {
+        event.destroy({
+          success: function (event) {
+            res.redirect('/dashboard');
+          },
+          error: function (event, error) {
+            res.send('Failed to save event, error code: ' + error.message);
+          }
+        });
+      },
+      error: function (error) {
+        res.redirect('/dashboard');
+      }
+    });
+  });
+  
+  // render event/edit
+  app.get('/:id/edit', function (req, res) {
+    var Event = Parse.Object.extend('Event');
+    var query = new Parse.Query(Event);
+    query.equalTo('createdBy', currUser);
+    query.get(req.params.id, {
+      success: function (event) {
+        res.render('event/edit', {event: event});
+      },
+      error: function (event, error) {
+        res.redirect('/dashboard');
+      }
+    });
+  });
+
+  // update event
+  app.post('/:id/edit', function (req, res) {
+    var Event = Parse.Object.extend('Event');
+    var query = new Parse.Query(Event);
+    query.equalTo('createdBy', currUser); // crated by curr user
+    query.get(req.params.id, {
+      success: function (event) {
+        event.set('eventName', req.body.eventName);
+        event.set('eventDescription', req.body.eventDescription);
+        event.save(null, {
+          success: function (event) {
+            res.redirect('/dashboard');
+          },
+          error: function (event, error) {
+            res.send('Failed to save event, with error code: ' + error.message);
+          }
+        });
+      },
+      error: function (event, error) {
+        res.redirect('/dashboard');
+      }
+    });
+  });
+
   return app;
-}(); // end function
+
+}(); // module.exports = function ()
