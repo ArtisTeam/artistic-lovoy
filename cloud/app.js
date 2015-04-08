be8eb1cbb52f92aa4823ca3fd0976c99c26a01ca
JW-CHEN <jic023@eng.ucsd.edu>
2015-04-08 13:48:07 -0700

    add volunteer profile with pointer retrival event name from enroll class

--- cloud/app.js
+++ cloud/app.js
@@ -26,12 +26,33 @@ app.get('/', function (req, res) {
 
 app.get('/profile', function (req, res) {
   var currUser = Parse.User.current();
+  var moment = require("cloud/lib/moment.js");
   if (currUser) {
     if (currUser.get('group') === 1) {
       res.render('org/profile');
     } else if (currUser.get('group') === 2) {
       alert('entered:' + currUser.get('group'));
-      res.render('vol/profile');
+      var Enroll = Parse.Object.extend('Enroll');
+      var query = new Parse.Query(Enroll);
+      query.equalTo('vol', currUser);
+      query.include('event');
+      query.find({
+        success: function (events) {
+          var eventPointedArr = new Array(events.length);
+          for (var i=0; i<events.length; ++i) {
+            events[i].createdAt = 
+              moment(events[i].createdAt).format("YYYY-MM-DD, hh:mm");
+            events[i].updatedAt = 
+              moment(events[i].updatedAt).format("YYYY-MM-DD, hh:mm");
+            eventPointedArr[i] = events[i].get('event');
+            //alert(eventPointedArr[i].get('name'));
+          }
+          res.render('vol/profile', {events: eventPointedArr});
+        }, 
+        error: function(error) {
+          res.send("Fail to query " + currUser + "events")
+        }
+      });
     }
   } else {
     res.redirect('/login?redir=profile');
