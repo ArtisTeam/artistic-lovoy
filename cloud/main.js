dd48762d57513f90bb6f7982233f534927e1fe47
Yixing Lao <linuxthink@gmail.com>
2015-04-07 19:44:18 -0700

    add beforesave framework in main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,28 @@
 require('cloud/app.js');
 
+var Enroll = Parse.Object.extend("Enroll");
+
+// Check if stopId is set, and enforce uniqueness based on the stopId column.
+Parse.Cloud.beforeSave("Enroll", function(req, res) {
+  // get requested vol and event
+  var reqVol = req.get("vol");
+  var reqEvent = req.get("event");
+  // query if duplicated data exists
+  var query = new Parse.Query(Enroll);
+  query.equalTo('vol', reqVol);
+  query.equalTo('event', reqEvent);
+  query.find({
+    success: function(women) {
+      // Do stuff
+    },
+    error: function(error) {
+      // Do stuff
+    }
+  }); // query.find
+}); // Parse.Cloud.beforeSave
+
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-// Parse.Cloud.define("hello", function(request, response) {
-//   response.success("Hello world!");
+// Parse.Cloud.define("hello", function(req, res) {
+//   res.success("Hello world!");
 // });
