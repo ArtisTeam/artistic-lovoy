13d3492d50dd2440fed4405368670218a6d35da1
Yixing Lao <linuxthink@gmail.com>
2015-04-10 22:17:00 -0700

    Update volunteer profile

--- cloud/profile.js
+++ cloud/profile.js
@@ -10,23 +10,7 @@ module.exports = function () {
       if (currUser.get('group') === 1) {
         res.render('org/profile');
       } else if (currUser.get('group') === 2) {
-        alert('entered:' + currUser.get('group'));
-        var Enroll = Parse.Object.extend('Enroll');
-        var query = new Parse.Query(Enroll);
-        query.equalTo('vol', currUser);
-        query.include('event');
-        query.find({
-          success: function (events) {
-            var eventPointedArr = new Array(events.length);
-            for (var i=0; i<events.length; ++i) {
-              eventPointedArr[i] = events[i].get('event');
-            }
-            res.render('vol/profile', {events: eventPointedArr});
-          },
-          error: function(error) {
-            res.send("Fail to query " + currUser + "events")
-          }
-        });
+        res.render('vol/profile');
       }
     } else {
       res.redirect('/login?redir=profile');
