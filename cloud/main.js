7e0c07f5f301cb1f77f0ef12d6c6378efa901960
Lirong Liu <ll2867@nyu.edu>
2015-04-22 02:16:08 -0400

    added twilio messaging service

--- cloud/main.js
+++ cloud/main.js
@@ -30,6 +30,30 @@ Parse.Cloud.beforeSave("Enroll", function(request, response) {
   }); // query.find
 }); // Parse.Cloud.beforeSave
 
+
+Parse.Cloud.define("sendTwilioMessage", function(request, response) {
+    // Require and initialize the Twilio module with your credentials
+    var client = require('twilio')('AC15300787442c39ec0b8bd2bae0e90bfd', '91f45717322eec04c8eb917b9a5d623a');
+     
+    console.log("request: " + request.params);
+    // Send an SMS message
+    client.sendSms({
+        to: request.params.phoneNumber,
+        from: '+17162496228', 
+        body: 'Hello ' + request.params.firstName + ', your verification code for Lovoy is ' + request.params.passcode
+      }, function(err, responseData) { 
+        if (err) {
+          console.log(err);
+        response.error("Twilio failed");
+        } else { 
+          console.log(responseData.from); 
+          console.log(responseData.body);
+          response.success("Twilio succeeded");
+        }
+      }
+    );
+});
+
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 // Parse.Cloud.define("hello", function(req, res) {
